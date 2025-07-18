c6a10754bd21d70b8dc8764fcb869d9c8b054188
shraddhagaur0607 <shraddhagaurjob0607@gmail.com>
2025-07-18 14:53:26 +0530

    initial setup and tailwind configured.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      Shraddha, future best developer
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
